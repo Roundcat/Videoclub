16093ec568dd7b47cb144e1aeafc8980bb1a6d82
Arnaud Charron <arnaudcharron@gmail.com>
2016-05-13 19:43:06 +0200

    added ActeurRealisateur Controller with ajouter and modifier actions

---
+++ application/controllers/ActeurRealisateurController.php
@@ -0,0 +1,32 @@
+<?php
+
+class ActeurRealisateurController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function ajouterAction()
+    {
+        // action body
+    }
+
+    public function modifierAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
