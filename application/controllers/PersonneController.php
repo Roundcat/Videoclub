25c20da12d8d423289f8483b643188609c7cb0bc
Arnaud Charron <arnaudcharron@gmail.com>
2016-05-14 16:39:28 +0200

    initialized personne files

---
+++ application/controllers/PersonneController.php
@@ -0,0 +1,53 @@
+<?php
+
+class PersonneController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function ajouterAction()
+    {
+        // action body
+    }
+
+    public function modifierAction()
+    {
+        // action body
+    }
+
+    public function consulterAction()
+    {
+        // action body
+    }
+
+    public function desactiveAction()
+    {
+        // action body
+    }
+
+    public function rechercherAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
