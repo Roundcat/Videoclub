2307f981bc370d4ab21c394432343b97a8e9f892
Arnaud Charron <arnaudcharron@gmail.com>
2016-05-14 15:55:50 +0200

    modified actaurRealisateur form file

--- application/forms/ActeurRealisateur.php
+++ application/forms/ActeurRealisateur.php
@@ -5,9 +5,24 @@ class Application_Form_ActeurRealisateur extends Zend_Form
 
     public function init()
     {
-        /* Form Elements & Other Definitions Here ... */
+      $this->setName('acteurRealisateur');
+
+      $id = new Zend_Form_Element_Hidden('id');
+      $id->addFilter('Int');
+
+      $genre = new Zend_Form_Element_Text('acteurRealisateur');
+      $genre->setLabel('Genre')
+              ->setRequired(true)
+              ->addFilter('StripTags')
+              ->addFilter('StringTrim')
+              ->addValidator('NotEmpty');
+
+      $envoyer = new Zend_Form_Element_Submit('envoyer');
+      $envoyer->setAttrib('id', 'boutonenvoyer');
+
+      $this->addElements(array($id, $genre, $envoyer));
+    }
     }
 
 
 }
-
