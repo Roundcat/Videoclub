fd2008550dd4ceb4d22c92328ddb92d7ee3dbca2
Arnaud Charron <arnaudcharron@gmail.com>
2016-05-14 16:43:00 +0200

    added function obtenirPersonne

--- application/models/DbTable/Personne.php
+++ application/models/DbTable/Personne.php
@@ -5,6 +5,15 @@ class Application_Model_DbTable_Personne extends Zend_Db_Table_Abstract
 
     protected $_name = 'personne';
 
+    public function obtenirPersonne($id)
+    {
+        $id = (int)$id;
+        $row = $this->fetchRow('id = ' . $id);
+        if (!$row) {
+            throw new Exception("Impossible de trouver l'enregistrement $id");
+        }
+        return $row->toArray();
+    }
 
+    
 }
-
