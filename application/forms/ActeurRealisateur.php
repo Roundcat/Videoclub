b4748c8a15133e6aa89f73c523467b3b8928e894
Arnaud Charron <arnaudcharron@gmail.com>
2016-05-14 19:16:41 +0200

    possibility to add acteur or realisateur -> done

--- application/forms/ActeurRealisateur.php
+++ application/forms/ActeurRealisateur.php
@@ -9,23 +9,23 @@ class Application_Form_ActeurRealisateur extends Zend_Form
       $id = new Zend_Form_Element_Hidden('id');
       $id->addFilter('Int');
 
-      $acteurRealisateur = new Zend_Form_Element_Text('nom');
-      $acteurRealisateur->setLabel('Nom')
-              ->setRequired(true)
-              ->addFilter('StripTags')
-              ->addFilter('StringTrim')
-              ->addValidator('NotEmpty');
+      $nom = new Zend_Form_Element_Text('nom');
+      $nom->setLabel('Nom')
+          ->setRequired(true)
+          ->addFilter('StripTags')
+          ->addFilter('StringTrim')
+          ->addValidator('NotEmpty');
 
-      $acteurRealisateur = new Zend_Form_Element_Text('prenom');
-      $acteurRealisateur->setLabel('Prénom')
-              ->setRequired(true)
-              ->addFilter('StripTags')
-              ->addFilter('StringTrim')
-              ->addValidator('NotEmpty');
+      $prenom = new Zend_Form_Element_Text('prenom');
+      $prenom->setLabel('Prénom')
+             ->setRequired(true)
+             ->addFilter('StripTags')
+             ->addFilter('StringTrim')
+             ->addValidator('NotEmpty');
 
       $envoyer = new Zend_Form_Element_Submit('envoyer');
       $envoyer->setAttrib('id', 'boutonenvoyer');
 
-      $this->addElements(array($id, $acteurRealisateur, $envoyer));
+      $this->addElements(array($id, $nom, $prenom, $envoyer));
     }
 }
