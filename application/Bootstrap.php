3dca4a7677f957d47b4b729a865465ed3adbaa38
Arnaud Charron <arnaudcharron@gmail.com>
2016-05-27 00:07:50 +0200

    design

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -20,7 +20,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     {
         $this->bootstrap('view');
         $view = $this->getResource('view');
-        $view->doctype('XHTML1_STRICT');
+        $view->doctype('HTML5');
     }
 
 }
