9f38d5126067ec7ef74690fa7fa1b4d98d7e1bd7
Arnaud Charron <arnaudcharron@gmail.com>
2016-05-29 22:41:14 +0200

    initialisation du modèle DbTable

--- application/models/DbTable/Personne.php
+++ application/models/DbTable/Personne.php
@@ -2,56 +2,13 @@
 
 class Application_Model_DbTable_Personne extends Zend_Db_Table_Abstract
 {
+
+    // Déclaration du nom de la table
     protected $_name = 'personne';
 
-    // public function obtenirPersonne($id)
-    // {
-    //     $id = (int)$id;
-    //     $row = $this->fetchRow('id = ' . $id);
-    //     if (!$row) {
-    //         throw new Exception("Impossible de trouver l'enregistrement $id");
-    //     }
-    //     return $row->toArray();
-    // }
-    //
-    // public function ajouterPersonne($nom, $prenom, $password, $courriel, $adresse1, $adresse2, $codePostal, $ville, $estEmploye)
-    // {
-    //     $data = array(
-    //         'nom'         =>  $nom,
-    //         'prenom'      =>  $prenom,
-    //         'motDePasse'  =>  $password,
-    //         'courriel'    =>  $courriel,
-    //         'adresse1'    =>  $adresse1,
-    //         'adresse2'    =>  $adresse2,
-    //         'code_postal' =>  $codePostal,
-    //         'ville'       =>  $ville,
-    //         'estEmploye'  =>  $estEmploye,
-    //     );
-    //     $this->insert($data);
-    // }
-    //
-    // public function modifierPersonne($id, $nom, $prenom, $password, $courriel, $adresse1, $adresse2, $codePostal, $ville, $estEmploye)
-    // {
-    //     $data = array(
-    //         'nom'         =>  $nom,
-    //         'prenom'      =>  $prenom,
-    //         'motDePasse'  =>  $password,
-    //         'courriel'    =>  $courriel,
-    //         'adresse1'    =>  $adresse1,
-    //         'adresse2'    =>  $adresse2,
-    //         'code_postal' =>  $codePostal,
-    //         'ville'       =>  $ville,
-    //         'estEmploye'  =>  $estEmploye,
-    //     );
-    //     $this->update($data, 'id = '. (int)$id);
-    // }
-    //
-    // public function desactivePersonne($id, $desactive)
-    // {
-    //   $desactive = 1;
-    //   $data = array(
-    //       'desactive'     =>  $desactive,
-    //   );
-    //   $this->update($data, 'id = '. (int)$id);
-    // }
+    public function getName()
+    {
+        return $this->_name;
+    }
+
 }
