4c7b8ae01f323e2d77cc75daa8493b09d8d24fbe
Arnaud Charron <arnaudcharron@gmail.com>
2016-05-29 16:28:17 +0200

    initialisation du controller

--- application/controllers/PersonneController.php
+++ application/controllers/PersonneController.php
@@ -6,10 +6,22 @@ class PersonneController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+        $this->_flashMessenger = $this->_helper
+                                      ->getHelper('FlashMessenger');
+        $this->initView();
     }
 
-
     public function indexAction()
+    {
+        // action body
+    }
+
+    public function listeClientAction()
+    {
+        // action body
+    }
+
+    public function listeEmployeAction()
     {
         // action body
     }
@@ -20,8 +32,8 @@ class PersonneController extends Zend_Controller_Action
      */
     public function listeAction()
     {
-        $personnes = new Application_Model_DbTable_Personne();
-        $this->view->personne = $personnes->fetchAll();
+        // $personnes = new Application_Model_DbTable_Personne();
+        // $this->view->personne = $personnes->fetchAll();
     }
 
     /**
@@ -32,45 +44,45 @@ class PersonneController extends Zend_Controller_Action
     public function ajouterAction()
     {
       // Instanciation de Application_Form_Personne
-      $form = new Application_Form_Personne();
-      // Affectation au bouton d'envoi le libellé 'Ajouter'
-      $form->envoyer->setLabel('Ajouter');
-      // Assignation du formulaire à la vue pour l'affichage
-      $this->view->form = $form;
-
-      // Si la méthode isPost() de l'objet de requête renvoie true, alors le formulaire a été envoyé
-      if ($this->getRequest()->isPost()) {
-        // Récupération des données de la requête avec la méthode getPost()
-        $formData = $this->getRequest()->getPost();
-        // Vérification que les données de la requête soient valides avec la méthode membre isValid()
-        if ($form->isValid($formData)) {
-          // Si le formulaire est valide
-          // instanciation de la classe modèle Application_Model_DbTable_ActeurRealisateur
-          // et utilisation de la méthode ajouterActeurRealisateur() créée dans la classe Application_Model_DbTable_ActeurRealisateur
-          // pour créer un nouvel enregistrment dans la base de données
-          $nom        = $form->getValue('nom');
-          $prenom     = $form->getValue('prenom');
-          $password   = $form->getValue('motDePasse');
-          $courriel   = $form->getValue('courriel');
-          $adresse1   = $form->getValue('adresse1');
-          $adresse2   = $form->getValue('adresse2');
-          $codePostal = $form->getValue('code_postal');
-          $ville      = $form->getValue('ville');
-          $estEmploye = $form->getValue('estEmploye');
-          $personnes  = new Application_Model_DbTable_Personne();
-      $personnes->ajouterPersonne($nom, $prenom, $password, $courriel, $adresse1, $adresse2, $codePostal, $ville, $estEmploye);
-
-          // Après avoir sauvegardé le nouvel enregistrement d'acteur réalisateur
-          // redirection vers l'action index avec l'aide d'action Redirector
-          // Ici retour vers la page d'accueil
-          $this->_helper->flashMessenger->addMessage('Personne ajoutée correctement');
-          $this->_helper->redirector('index');
-          // Si les données du formulaire ne sont pas valides
-          // nouvel affichage du formulaire avec les données fournies
-        } else {
-          $form->populate($formData);
-        }
-      }
+    //   $form = new Application_Form_Personne();
+    //   // Affectation au bouton d'envoi le libellé 'Ajouter'
+    //   $form->envoyer->setLabel('Ajouter');
+    //   // Assignation du formulaire à la vue pour l'affichage
+    //   $this->view->form = $form;
+      //
+    //   // Si la méthode isPost() de l'objet de requête renvoie true, alors le formulaire a été envoyé
+    //   if ($this->getRequest()->isPost()) {
+    //     // Récupération des données de la requête avec la méthode getPost()
+    //     $formData = $this->getRequest()->getPost();
+    //     // Vérification que les données de la requête soient valides avec la méthode membre isValid()
+    //     if ($form->isValid($formData)) {
+    //       // Si le formulaire est valide
+    //       // instanciation de la classe modèle Application_Model_DbTable_ActeurRealisateur
+    //       // et utilisation de la méthode ajouterActeurRealisateur() créée dans la classe Application_Model_DbTable_ActeurRealisateur
+    //       // pour créer un nouvel enregistrment dans la base de données
+    //       $nom        = $form->getValue('nom');
+    //       $prenom     = $form->getValue('prenom');
+    //       $password   = $form->getValue('motDePasse');
+    //       $courriel   = $form->getValue('courriel');
+    //       $adresse1   = $form->getValue('adresse1');
+    //       $adresse2   = $form->getValue('adresse2');
+    //       $codePostal = $form->getValue('code_postal');
+    //       $ville      = $form->getValue('ville');
+    //       $estEmploye = $form->getValue('estEmploye');
+    //       $personnes  = new Application_Model_DbTable_Personne();
+    //   $personnes->ajouterPersonne($nom, $prenom, $password, $courriel, $adresse1, $adresse2, $codePostal, $ville, $estEmploye);
+      //
+    //       // Après avoir sauvegardé le nouvel enregistrement d'acteur réalisateur
+    //       // redirection vers l'action index avec l'aide d'action Redirector
+    //       // Ici retour vers la page d'accueil
+    //       $this->_helper->flashMessenger->addMessage('Personne ajoutée correctement');
+    //       $this->_helper->redirector('index');
+    //       // Si les données du formulaire ne sont pas valides
+    //       // nouvel affichage du formulaire avec les données fournies
+    //     } else {
+    //       $form->populate($formData);
+    //     }
+    //   }
     }
 
     public function modifierAction()
