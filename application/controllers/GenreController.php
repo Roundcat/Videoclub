faf735fe1f34f2aa3ddfca12f6e0bf6886081376
Arnaud Charron <arnaudcharron@gmail.com>
2016-05-29 16:25:47 +0200

    changement de nom de variable

--- application/controllers/GenreController.php
+++ application/controllers/GenreController.php
@@ -18,27 +18,12 @@ class GenreController extends Zend_Controller_Action
 
     public function listeAction()
     {
-        $genre = new Application_Model_GenreMapper();
-        $this->view->entries = $genre->fetchAll();
+        $mapper = new Application_Model_GenreMapper();
+        $this->view->entries = $mapper->fetchAll();
     }
 
     public function ajouterAction()
     {
-    //   $form = new Application_Form_Genre();
-    //   $form->envoyer->setLabel('Ajouter');
-    //   $this->view->form = $form;
-    //   if ($this->getRequest()->isPost()) {
-    //       $formData = $this->getRequest()->getPost();
-    //       if ($form->isValid($formData)) {
-    //           $genre = $form->getValue('genre');
-    //           $genres = new Application_Model_DbTable_Genre();
-    //           $genres->ajouterGenre($genre);
-    //           $this->_helper->redirector('index');
-    //       } else {
-    //           $form->populate($formData);
-    //       }
-    //   }
-    //
         $request = $this->getRequest();
         $form    = new Application_Form_Genre();
         $form->submit->setLabel('Ajouter');
@@ -60,27 +45,6 @@ class GenreController extends Zend_Controller_Action
 
     public function modifierAction()
     {
-    //   $form = new Application_Form_Genre();
-    //   $form->envoyer->setLabel('Sauvegarder');
-    //   $this->view->form = $form;
-    //   if ($this->getRequest()->isPost()) {
-    //       $formData = $this->getRequest()->getPost();
-    //       if ($form->isValid($formData)) {
-            //   $id = $form->getValue('id');
-            //   $genre = $form->getValue('genre');
-    //           $genres = new Application_Model_DbTable_Genre();
-    //           $genres->modifierGenre($id, $genre);
-    //           $this->_helper->redirector('index');
-    //       } else {
-    //           $form->populate($formData);
-    //       }
-    //   } else {
-    //       $id = $this->_getParam('id', 0);
-    //       if ($id > 0) {
-    //           $genres = new Application_Model_DbTable_Genre();
-    //           $form->populate($genres->obtenirGenre($id));
-    //       }
-    //   }
         $request = $this->getRequest();
         $form    = new Application_Form_Genre();
         $form->submit->setLabel('Modifier');
