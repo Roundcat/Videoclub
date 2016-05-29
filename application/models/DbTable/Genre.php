db2f79d1407635ee633343341a2c228c255c86cf
Arnaud Charron <arnaudcharron@gmail.com>
2016-05-29 22:40:15 +0200

    suppression de code commenté

--- application/models/DbTable/Genre.php
+++ application/models/DbTable/Genre.php
@@ -14,35 +14,4 @@ class Application_Model_DbTable_Genre extends Zend_Db_Table_Abstract
         return $this->_name;
     }
 
-    // public function obtenirGenre($id)
-    // {
-    //     $id = (int)$id;
-    //     $row = $this->fetchRow('id = ' . $id);
-    //     if (!$row) {
-    //         throw new Exception("Impossible de trouver l'enregistrement $id");
-    //     }
-    //     return $row->toArray();
-    // }
-    //
-    // public function ajouterGenre($genre)
-    // {
-    //     $data = array(
-    //         'genre' => $genre,
-    //     );
-    //     $this->insert($data);
-    // }
-    //
-    // public function modifierGenre($id, $genre)
-    // {
-    //     $data = array(
-    //         'genre' => $genre,
-    //     );
-    //     $this->update($data, 'id = '. (int)$id);
-    // }
-    //
-    // public function supprimerGenre($id)
-    // {
-    //     $this->delete('id =' . (int)$id);
-    // }
-
 }
