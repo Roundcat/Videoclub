6825f6dcc0fdbb6b42b00c3368c67be556961f3a
Arnaud Charron <arnaudcharron@gmail.com>
2016-05-30 23:54:56 +0200

    suppression de code commenté

--- application/forms/Genre.php
+++ application/forms/Genre.php
@@ -1,22 +1,7 @@
 <?php
 class Application_Form_Genre extends Zend_Form
 {
-    // public function init()
-    // {
-    //   $this->setName('genre');
-    //   $id = new Zend_Form_Element_Hidden('id');
-    //   $id->addFilter('Int');
-    //   $genre = new Zend_Form_Element_Text('genre');
-    //   $genre->setLabel('Genre')
-    //           ->setRequired(true)
-    //           ->addFilter('StripTags')
-    //           ->addFilter('StringTrim')
-    //           ->addValidator('NotEmpty');
-    //   $envoyer = new Zend_Form_Element_Submit('envoyer');
-    //   $envoyer->setAttrib('id', 'boutonenvoyer');
-    //   $this->addElements(array($id, $genre, $envoyer));
-    // }
-    //
+
     public function init()
     {
         // La méthode HTTP d'envoi du formulaire
