01d9579704dadd30e5aedf8cbd3ec7ed3f54d378
Arnaud Charron <arnaudcharron@gmail.com>
2016-06-02 22:00:42 +0200

    écriture de la fonction ajouterPersonne()

--- application/models/PersonneMapper.php
+++ application/models/PersonneMapper.php
@@ -52,6 +52,7 @@ class Application_Model_PersonneMapper
         $sql = "SELECT  id
                         , prenom
                         , nom
+                        , numero_adherent
                 FROM    personne
                 WHERE   estemploye = 0
                 ORDER BY nom ASC, prenom ASC;";
@@ -64,7 +65,8 @@ class Application_Model_PersonneMapper
             $client = new Application_Model_Personne();
             $client ->setId($row['id'])
                     ->setPrenom($row['prenom'])
-                    ->setNom($row['nom']);
+                    ->setNom($row['nom'])
+                    ->setNumeroAdherent($row['numero_adherent']);
 
             $tab[] = $client;
         }
@@ -76,6 +78,7 @@ class Application_Model_PersonneMapper
         $sql = "SELECT  id
                         , prenom
                         , nom
+                        , numero_adherent
                 FROM    personne
                 WHERE   estemploye = 1
                 ORDER BY nom ASC, prenom ASC;";
@@ -88,7 +91,8 @@ class Application_Model_PersonneMapper
             $employe = new Application_Model_Personne();
             $employe ->setId($row['id'])
                     ->setPrenom($row['prenom'])
-                    ->setNom($row['nom']);
+                    ->setNom($row['nom'])
+                    ->setNumeroAdherent($row['numero_adherent']);
 
             $tab[] = $employe;
         }
@@ -102,6 +106,7 @@ class Application_Model_PersonneMapper
         foreach ($resultSet as $row) {
             $entry = new Application_Model_Personne();
             $entry->setId($row->id)
+                  ->setNumeroAdherent($row['numero_adherent'])
                   ->setPrenom($row->prenom)
                   ->setNom($row->nom)
                   ->setEstEmploye($row->estEmploye);
@@ -110,18 +115,42 @@ class Application_Model_PersonneMapper
         return $entries;
     }
 
-    public function modifierPersonne($id, $prenom, $nom, $courriel, $adresse1, $adresse2, $codePostal, $ville, $password, $estEmploye)
+    public function ajouterPersonne(Application_Model_Personne $personne)
+    {
+        $data = array(
+            'prenom'        =>  $personne->getPrenom(),
+            'nom'           =>  $personne->getNom(),
+            'courriel'      =>  $personne->getCourriel(),
+            'motDePasse'    =>  $personne->getPassword(),
+            'adresse1'      =>  $personne->getAdresse1(),
+            'adresse2'      =>  $personne->getAdresse2(),
+            'code_postal'   =>  $personne->getCodePostal(),
+            'ville'         =>  $personne->getVille(),
+            'estEmploye'    =>  $personne->getEstEmploye(),
+            'desactive'    =>  $personne->getDesactive(),
+            'date_creation'    =>  $personne->getDateCreation(),
+            'numero_adherent'    =>  $personne->getNumeroAdherent(),
+        );
+        if (null === ($id = $personne->getId())) {
+            unset($data['id']);
+            $this->getDbTable()->insert($data);
+        } /*else {
+            $this->getDbTable()->update($data, array('id = ?' => $id));
+        }*/
+    }
+
+    public function modifierPersonne($id, $prenom, $nom/*, $courriel, $adresse1, $adresse2, $codePostal, $ville, $password, $estEmploye*/)
     {
         $data = array(
             'prenom'            =>  $prenom,
             'nom'               =>  $nom,
-            'courriel'          =>  $courriel,
-            'adresse1'          =>  $adresse1,
-            'adresse2'          =>  $adresse2,
-            'code_postal'       =>  $codePostal,
-            'ville'             =>  $ville,
-            'motDePasse'        =>  $password,
-            'estEmploye'        =>  $estEmploye,
+            // 'courriel'          =>  $courriel,
+            // 'adresse1'          =>  $adresse1,
+            // 'adresse2'          =>  $adresse2,
+            // 'code_postal'       =>  $codePostal,
+            // 'ville'             =>  $ville,
+            // 'motDePasse'        =>  $password,
+            // 'estEmploye'        =>  $estEmploye,
         );
         $this->getDbTable()->update($data, array('id = ?' => $id));
     }
