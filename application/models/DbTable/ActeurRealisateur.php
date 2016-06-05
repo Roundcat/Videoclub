8e3c1ef29739c2c551d1b5662b820753db810694
Arnaud Charron <arnaudcharron@gmail.com>
2016-06-05 16:08:44 +0200

    paramétrage correct du fichier

--- application/models/DbTable/ActeurRealisateur.php
+++ application/models/DbTable/ActeurRealisateur.php
@@ -5,32 +5,9 @@ class Application_Model_DbTable_ActeurRealisateur extends Zend_Db_Table_Abstract
 
     protected $_name = 'acteur_realisateur';
 
-    // public function obtenirActeurRealisateur($id)
-    // {
-    //     $id = (int)$id;
-    //     $row = $this->fetchRow('id = ' . $id);
-    //     if (!$row) {
-    //         throw new Exception("Impossible de trouver l'enregistrement $id");
-    //     }
-    //     return $row->toArray();
-    // }
-    //
-    // public function ajouterActeurRealisateur($nom, $prenom)
-    // {
-    //     $data = array(
-    //         'nom' => $nom,
-    //         'prenom' => $prenom,
-    //     );
-    //     $this->insert($data);
-    // }
-    //
-    // public function modifierActeurRealisateur($id, $nom, $prenom)
-    // {
-    //     $data = array(
-    //         'nom' => $nom,
-    //         'prenom' => $prenom,
-    //     );
-    //     $this->update($data, 'id = '. (int)$id);
-    // }
+    public function getName()
+    {
+        return $this->_name;
+    }
 
 }
