453bf3491c824e23ded14fb4d8f311beeedb65f9
Arnaud Charron <arnaudcharron@gmail.com>
2016-06-05 16:12:11 +0200

    écriture script fonction ajouterActeurRealisateur, modifierActeurRealisateur, obtenirActeurRealisateur et fetchAll

--- application/models/ActeurRealisateurMapper.php
+++ application/models/ActeurRealisateurMapper.php
@@ -32,6 +32,57 @@ class Application_Model_ActeurRealisateurMapper
         return $this->_dbTable;
     }
 
+    public function ajouterActeurRealisateur(Application_Model_ActeurRealisateur $acteurRealisateur)
+    {
+        $data = array(
+            'prenom'    => $acteurRealisateur->getPrenom(),
+            'nom'       => $acteurRealisateur->getNom(),
+        );
+        if (null === ($id = $acteurRealisateur->getId())) {
+            unset($data['id']);
+            $this->getDbTable()->insert($data);
+        } else {
+            $this->getDbTable()->update($data, array('id = ?' => $id));
+        }
+    }
 
+    public function modifierActeurRealisateur($id, $prenom, $nom)
+    {
+        $data = array(
+            'prenom'            =>  $prenom,
+            'nom'               =>  $nom,
+        );
+        $this->getDbTable()->update($data, array('id = ?' => $id));
+    }
+
+    public function obtenirActeurRealisateur($id)
+    {
+        $row = $this->_db->fetchRow('SELECT * FROM '.$this->getDbTable()->getName().' WHERE `id` = '.$id.' ');
+        if($row){
+            $acteurRealisateur = new Application_Model_ActeurRealisateur();
+            $acteurRealisateur->setId($row['id']);
+            $acteurRealisateur->setPrenom($row['prenom']);
+            $acteurRealisateur->setNom($row['nom']);
+
+            return $acteurRealisateur;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public function fetchAll()
+    {
+        $resultSet = $this->getDbTable()->fetchAll();
+        $entries   = array();
+        foreach ($resultSet as $row) {
+            $entry = new Application_Model_ActeurRealisateur();
+            $entry->setId($row->id)
+                  ->setPrenom($row->prenom)
+                  ->setNom($row->nom);
+            $entries[] = $entry;
+        }
+        return $entries;
+    }
 
 }
