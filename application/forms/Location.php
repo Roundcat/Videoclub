866a92a047481515a3cfe4c1ab51ace2d127b6cb
Arnaud Charron <arnaudcharron@gmail.com>
2016-06-11 16:11:18 +0200

    création du formulaire

--- application/forms/Location.php
+++ application/forms/Location.php
@@ -5,9 +5,65 @@ class Application_Form_Location extends Zend_Form
 
     public function init()
     {
-        /* Form Elements & Other Definitions Here ... */
+        // La méthode HTTP d'envoi du formulaire
+        $this->setMethod('post');
+
+        // Un élément personne
+        $this->addElement('select', 'Personne_id', array(
+            'label'         =>  'Client : ',
+            'required'      =>  true,
+            'filters'       =>  array('StripTags', 'StringTrim'),
+            'attribs'       =>  array('style' => 'width: 150px'),
+            'validators'    =>  array(array('validator' => 'NotEmpty'))
+        ));
+
+        // Un élément film
+        $this->addElement('select', 'media_id', array(
+            'label'         =>  'Film : ',
+            'required'      =>  true,
+            'filters'       =>  array('StripTags', 'StringTrim'),
+            'attribs'       =>  array('style' => 'width: 150px'),
+            'validators'    =>  array(array('validator' => 'NotEmpty'))
+        ));
+
+        // Un élément date_retour
+        $this->addElement('text', 'date_retour', array(
+            'label'         =>  'Date de retour de la location : ',
+            'required'      =>  true,
+            'filters'       =>  array('StripTags', 'StringTrim'),
+            'attribs'       =>  array('size' => 20),
+            'validators'    =>  array(array('validator' =>  'NotEmpty',
+                                                            'Date'))
+        ));
+
+        // Un bouton d'envoi
+        $this->addElement('submit', 'submit', array(
+            'ignore'   => true,
+            'label'    => 'Sauvegarder',
+        ));
+
+        // Un bouton d'annulation
+        $this->addElement('reset', 'reset', array(
+            'ignore'   => true,
+            'label'    => 'Annuler',
+        ));
+
+        // Et une protection anti CSRF
+        $this->addElement('hash', 'csrf', array(
+            'ignore' => true,
+        ));
+
+        $this->getElement("submit")->removeDecorator('DtDdWrapper') ;
+        $this->getElement("csrf")->removeDecorator('label') ;
+
+        $elements = array ("Personne_id", "media_id", "date_retour") ;
+
+        foreach ($elements as $element) {
+            $element = $this->getElement($element) ;
+            if ($element!=null) {
+                $element->removeDecorator('htmlTag');
+            }
+        }
     }
 
-
 }
-
