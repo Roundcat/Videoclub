43a8bb9f73b7c7744c90b7c139722265482f8988
Arnaud Charron <arnaudcharron@gmail.com>
2016-06-11 15:54:13 +0200

    ecriture script fonction sairSortie dans le controller

--- application/controllers/LocationController.php
+++ application/controllers/LocationController.php
@@ -20,7 +20,51 @@ class LocationController extends Zend_Controller_Action
 
     public function saisirSortieAction()
     {
-        // action body
+        $request    = $this->getRequest();
+        $form       = new Application_Form_Location();
+        $form->submit->setLabel('Ajouter');
+
+        // Affocher les films dans une liste déroulante
+        $filmMapper = new Application_Model_FilmMapper();
+        $films = $filmMapper->fetchAll();
+        $options = array();
+        foreach ($films as $film) {
+            $value = $film->getNom();
+            $options[$film->getId()] = $value;
+        }
+        $element = $form->getElement('realisateurId');
+        $element->setMultiOptions($options);
+
+        $clientMapper = new Application_Model_PersonneMapper();
+        $clients = $clientMapper->obtenirAllClients();
+        $options = array();
+        foreach ($clients as $client) {
+            $value = $client->getPrenom() . " " . $client->getNom();
+            $options[$client->getId()] = $value;
+        }
+        $element = $form->getElement('Personne_id');
+        $element->setMultiOptions($options);
+
+        if ($request->isPost()) {
+            $formData = $this->getRequest()->getPost();
+            if ($form->isValid($formData)) {
+                $location = new Application_Model_Location($form->getValues());
+
+                $dateLocation = date("d-m-Y");
+                $location->setDateLocation($dateLocation);
+
+                $mapper   = new Application_Model_LocationMapper();
+
+                $mapper->saisirSortieLocation($location);
+                return $this->_helper->redirector('index', 'Index');
+            } else {
+                $form->populate($formData);
+            }
+        }
+
+        $this->view->form = $form;
+    }
+
     }
 
     public function saisirRetourAction()
