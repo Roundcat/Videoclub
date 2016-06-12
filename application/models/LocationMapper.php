938d8ee7bad0796ec6ea8e64212aba4f160d7070
Arnaud Charron <arnaudcharron@gmail.com>
2016-06-12 23:57:48 +0200

    écriture script fonction pour obtenir toutes les sorties en cours

--- application/models/LocationMapper.php
+++ application/models/LocationMapper.php
@@ -39,11 +39,11 @@ class Application_Model_LocationMapper
             'media_id'          =>  $location->getMediaId(),
             'date_location'     =>  $location->getDateLocation(),
         );
-        if (null === ($id = $film->getId())) {
-            unset($data['id']);
+        if (null === ($idlocation = $location->getId())) {
+            unset($data['idlocation']);
             $this->getDbTable()->insert($data);
         } else {
-            $this->getDbTable()->update($data, array('id = ?' => $id));
+            $this->getDbTable()->update($data, array('idlocation = ?' => $idlocation));
         }
     }
 
@@ -52,6 +52,44 @@ class Application_Model_LocationMapper
         $data = array(
             'date_retour'     =>  $dateRetour,
         );
-        $this->getDbTable()->update($data, array('id = ?' => $id));
+        $this->getDbTable()->update($data, array('idlocation = ?' => $idlocation));
+    }
+
+    public function obtenirAllSorties()
+    {
+        $sql = "SELECT 	    l.idlocation AS idLocation
+                    	    , p.prenom AS prenomPersonne
+                            , p.nom AS nomPersonne
+                            , p.numero_adherent AS numeroAdherent
+                            , f.nom AS nomFilm
+                            , l.date_location AS dateLocation
+                FROM        location l
+                INNER JOIN  media m     ON m.id = l.media_id
+                INNER JOIN  film f      ON f.id = m.Film_idFilm
+                INNER JOIN  personne p  ON p.id = l.Personne_id
+                WHERE       ISNULL(date_retour)
+                ORDER BY l.date_location";
+
+        $recup = $this->_db->fetchAll($sql);
+
+        $tab = array();
+
+        foreach ($recup as $row) {
+            $location = new Application_Model_Location();
+            $location   ->setId($row['idLocation'])
+                        ->setDateLocation($row['dateLocation']);
+            $tab[] = $location;
+
+            $personne = new Application_Model_Personne();
+            $personne   ->setPrenom($row['prenomPersonne'])
+                        ->setNom($row['nomPersonne'])
+                        ->setNumeroAdherent($row['numeroAdherent']);
+            $tab[] = $personne;
+
+            $film     = new Application_Model_Film();
+            $film       ->setNom($row['nomFilm']);
+            $tab[] = $film;
+        }
+        return $tab;
     }
 }
