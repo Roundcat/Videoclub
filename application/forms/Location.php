e0fadb672ab70cf9573fc221ab553244e3ebfd72
Arnaud Charron <arnaudcharron@gmail.com>
2016-06-13 13:58:34 +0200

    modification du formulaire avec suppression de l'élément media car inutile et passage required à false pour DateRetour

--- application/forms/Location.php
+++ application/forms/Location.php
@@ -9,7 +9,7 @@ class Application_Form_Location extends Zend_Form
         $this->setMethod('post');
 
         // Un élément personne
-        $this->addElement('select', 'Personne_id', array(
+        $this->addElement('select', 'PersonneId', array(
             'label'         =>  'Client : ',
             'required'      =>  true,
             'filters'       =>  array('StripTags', 'StringTrim'),
@@ -17,19 +17,10 @@ class Application_Form_Location extends Zend_Form
             'validators'    =>  array(array('validator' => 'NotEmpty'))
         ));
 
-        // Un élément film
-        $this->addElement('select', 'media_id', array(
-            'label'         =>  'Film : ',
-            'required'      =>  true,
-            'filters'       =>  array('StripTags', 'StringTrim'),
-            'attribs'       =>  array('style' => 'width: 150px'),
-            'validators'    =>  array(array('validator' => 'NotEmpty'))
-        ));
-
         // Un élément date_retour
-        $this->addElement('text', 'date_retour', array(
+        $this->addElement('text', 'DateRetour', array(
             'label'         =>  'Date de retour de la location : ',
-            'required'      =>  true,
+            'required'      =>  false,
             'filters'       =>  array('StripTags', 'StringTrim'),
             'attribs'       =>  array('size' => 20),
             'validators'    =>  array(array('validator' =>  'NotEmpty',
